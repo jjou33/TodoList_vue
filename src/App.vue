354f4f0dfe988079a0d75e34814c701217ad822a
hyunwoo <zanda33@naver.com>
2022-05-18 22:23:07 +0900

    FEAT: vue todoList 프로젝트 환경 구성 업데이트

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+
+export default {};
+</script>
+
+<style></style>
