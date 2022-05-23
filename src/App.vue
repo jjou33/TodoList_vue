abf6ca17788b285d98eda6057dafc6435b16c7a7
hyunwoo <zanda33@naver.com>
2022-05-23 21:23:58 +0900

    Refactor: TodoFooter Clear 기능 App 으로 이동
    - 기존 Todo 컴포넌트에서 작동했던 기능들을 App 으로 이동하여 한곳에서 상태관리 가능하도록
      변경 완료

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       v-bind:propsdata="todoItems"
       v-on:removeItem="removeOneItem"
     ></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
   </div>
 </template>
 
@@ -17,6 +17,17 @@ import TodoList from "./components/TodoList.vue";
 import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
+        }
+      }
+    }
+  },
   data: function() {
     return {
       todoItems: []
@@ -31,19 +42,13 @@ export default {
     removeOneItem: function(todoItem, index) {
       localStorage.removeItem(todoItem.item);
       this.todoItems.splice(index, 1);
+    },
+    clearAllItems: function() {
+      localStorage.clear();
+      this.todoItems = [];
     }
   },
-  created: function() {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
-          this.todoItems.push(
-            JSON.parse(localStorage.getItem(localStorage.key(i)))
-          );
-        }
-      }
-    }
-  },
+
   components: {
     TodoHeader,
     TodoInput,
